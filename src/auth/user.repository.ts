259846ba7a2b73f5b3d9dbf60f60b9dbb170e423
Manuel Da Silva <manueldasilva98@gmail.com>
2020-10-26 16:05:07 -0400

    autenticacion avanzada

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,41 +1,41 @@
-import { EntityRepository, Repository } from "typeorm";
-import { AuthCompanySignUpDto } from "./dto/auth-company-signup.dto";
-import { AuthPersonSignUpDto } from "./dto/auth-person-signup.dto";
-import { User } from "./user.entity";
+import { EntityRepository, Repository } from 'typeorm';
+import { AuthCompanySignUpDto } from './dto/auth-company-signup.dto';
+import { AuthPersonSignUpDto } from './dto/auth-person-signup.dto';
+import { User } from './user.entity';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
-    
+  async companySignUp(
+    authCompanySignUpDto: AuthCompanySignUpDto,
+  ): Promise<void> {
+    const user = new User();
 
-    async companySignUp(authCompanySignUpDto: AuthCompanySignUpDto): Promise<void> {
+    this.buildBaseUSer(user, authCompanySignUpDto);
+    user.companyName = authCompanySignUpDto.companyName ?? null;
+    user.isCompany = true;
 
-        const user = new User();
+    await user.save();
+  }
 
-        this.buildBaseUSer(user, authCompanySignUpDto);
-        user.companyName = authCompanySignUpDto.companyName ?? null;
-        user.isCompany = true;
-        
-        await user.save();
-    }
+  async personSignUp(authPersonSignUpDto: AuthPersonSignUpDto): Promise<void> {
 
-    async personSignUp(authPersonSignUpDto: AuthPersonSignUpDto): Promise<void> {
+    const user = new User();
+    this.buildBaseUSer(user, authPersonSignUpDto);
+    user.firstName = authPersonSignUpDto.firstName ?? null;
+    user.lastName = authPersonSignUpDto.lastName ?? null;
+    user.isAdmin = authPersonSignUpDto.isAdmin;
+    user.isCompany = false;
+    await user.save();
+  }
 
-        const user = new User();
+  private buildBaseUSer(
+    user: User,
+    authDto: AuthCompanySignUpDto | AuthPersonSignUpDto,
+  ): void {
 
-        this.buildBaseUSer(user, authPersonSignUpDto);
-
-        user.firstName = authPersonSignUpDto.firstName ?? null;
-        user.lastName = authPersonSignUpDto.lastName ?? null;
-        user.isAdmin = authPersonSignUpDto.isAdmin;
-        user.isCompany = false;
-        await user.save();
-    }
-
-    private buildBaseUSer(user: User, authDto: AuthCompanySignUpDto | AuthPersonSignUpDto ): void {
-
-        user.email = authDto.email;
-        user.username = authDto.username;
-        user.password = authDto.username;
-        user.salt = '';
-    }
+    user.email = authDto.email;
+    user.username = authDto.username;
+    user.password = authDto.username;
+    user.salt = '';
+  }
 }
