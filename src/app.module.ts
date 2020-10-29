d09b49747e2b3a6811723f9472e01dafd478b1dc
Manuel Da Silva <manueldasilva98@gmail.com>
2020-10-29 19:25:38 -0400

    avance

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/typeorm.config';
 import { AuthModule } from './auth/auth.module';
+import { PaymentModule } from './payment/payment.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(typeOrmConfig),
-    AuthModule
+    AuthModule,
+    PaymentModule
   ],
   controllers: [],
   providers: [],
